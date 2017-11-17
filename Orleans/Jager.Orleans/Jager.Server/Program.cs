ee0b29c5c296319e45078cbf302ef78d62c91be6
André Jager <andre@sieland-jager.de>
2017-11-17 16:26:43 +0100

    First basic Orleans Host Server

---
+++ Orleans/Jager.Orleans/Jager.Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Orleans.Runtime.Configuration;
+using Orleans.Runtime.Host;
+
+namespace Jager.Server
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var siloConfig = ClusterConfiguration.LocalhostPrimarySilo();
+            var silo = new SiloHost("Test Silo", siloConfig);
+            silo.InitializeOrleansSilo();
+            
+            if (silo.StartOrleansSilo())
+                Console.WriteLine("Started");
+            
+            Console.WriteLine("Press Enter to close.");
+// wait here
+            Console.ReadLine();
+
+// shut the silo down after we are done.
+            silo.ShutdownOrleansSilo();
+        }
+    }
+}
